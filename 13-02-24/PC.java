c4325f586ea25fa13c3f0739cfaeae3571afc24c
pkcs210 <153653654+pkcs210@users.noreply.github.com>
2024-02-24 20:38:52 +0530

    Update PC.java

--- 13-02-24/PC.java
+++ 13-02-24/PC.java
@@ -1,1 +1,126 @@
+// Inter-Process Communication
 
+class Q {
+    private int n;
+    private boolean valueSet = false;
+
+    synchronized int get() {
+        while (!valueSet) {
+            try {
+                System.out.println("\nConsumer waiting\n");
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException caught");
+            }
+        }
+
+        System.out.println("Got: " + n);
+        valueSet = false;
+        System.out.println("\nNotify Producer\n");
+        notify();
+        return n;
+    }
+
+    synchronized void put(int n) {
+        while (valueSet) {
+            try {
+                System.out.println("\nProducer waiting\n");
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException caught");
+            }
+        }
+
+        this.n = n;
+        valueSet = true;
+        System.out.println("Put: " + n);
+        System.out.println("\nNotify Consumer\n");
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    private Q q;
+
+    Producer(Q q) {
+        this.q = q;
+        new Thread(this, "Producer").start();
+    }
+
+    public void run() {
+        int i = 0;
+        while (i < 15) {
+            q.put(i++);
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    private Q q;
+
+    Consumer(Q q) {
+        this.q = q;
+        new Thread(this, "Consumer").start();
+    }
+
+    public void run() {
+        int i = 0;
+        while (i < 15) {
+            int r = q.get();
+            System.out.println("Consumed: " + r);
+            i++;
+        }
+    }
+}
+
+public class PC {
+    public static void main(String args[]) {
+        Q q = new Q();
+        new Producer(q);
+        new Consumer(q);
+        System.out.println("Press Control-C to stop.");
+    }
+}
+
+// Deadlock
+public class DeadlockExample {
+    public static void main(String[] args) {
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+
+        Thread thread1 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("Thread 1 acquired lock1");
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (lock2) {
+                    System.out.println("Thread 1 acquired lock2");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println("Thread 2 acquired lock2");
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (lock1) {
+                    System.out.println("Thread 2 acquired lock1");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
