6b70ee07f598f4c75ea63f69a54501d6dc3ef8f2
pkcs210 <153653654+pkcs210@users.noreply.github.com>
2024-02-24 20:35:11 +0530

    Update Main.java

--- 06-02-24/Main.java
+++ 06-02-24/Main.java
@@ -1,1 +1,38 @@
+class CollegeThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("BMS College of Engineering");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class DepartmentThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("CSE");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        CollegeThread collegeThread = new CollegeThread();
+        DepartmentThread departmentThread = new DepartmentThread();
+
+        collegeThread.start();
+        departmentThread.start();
+    }
+}
 
