66f55bc004b2abf41be6addff0207ed6a9b634c6
pkcs210 <153653654+pkcs210@users.noreply.github.com>
2024-02-24 19:08:00 +0530

    Update StudentDemo.java

--- 12-12-23/StudentDemo.java
+++ 12-12-23/StudentDemo.java
@@ -1,1 +1,23 @@
-//Test
+import java.util.*;
+class QuadraticEquation {
+  public static void main(String args[]) {
+    Scanner s = new Scanner(System.in);
+    double a, b, c;
+    double D;
+    double r1, r2;
+
+    System.out.print("Enter a, b, & c:");
+    a = s.nextDouble();
+    b = s.nextDouble();
+    c = s.nextDouble();
+
+    D = b*b - 4*a*c;
+    
+  
+  
+  
+  }
+  
+
+
+}
